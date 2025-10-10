6e29272c65c8bcc70a310d8eec3404c4f55cc8fa
HammadUllah25 <49758382+HammadUllah25@users.noreply.github.com>
2025-10-10 19:26:14 +0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/job-board/",
+  base: "/job-board-react1/",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
